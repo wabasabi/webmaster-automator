e9ad65c2c788f14da868cfbae2444df1df9c9b1e
wabasabi <rhwabasabi@gmail.com>
2016-02-10 16:25:54 +0000

    Added the main program file

---
+++ automator.py
@@ -0,0 +1,15 @@
+'''
+Author: Ryan Harrington
+Date: 02/10/16
+File: automator.py
+Description: This program takes in multiple parameters and uses
+             them to generate facebook posts regarding any
+             upcoming art galleries for my job at the
+             McFarland Student Union Building.
+'''
+
+def main():
+    print "test"
+    
+if __name__ == '__main__':
+    main()
